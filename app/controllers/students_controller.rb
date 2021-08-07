bb559d2942f474bcefa742175d0da7fe80e8c32b
Megan McCarty <megan_mccarty@hotmail.com>
2021-08-07 12:13:22 -0400

    commit tree 4eda50af6df5ec89ff6939df318ceff34fbcb10b

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,10 @@
 class StudentsController < ApplicationController
-
-  def index
-    students = Student.all
-    render json: students
-  end
-
+    def index
+        students = Student.all
+        render json: students
+    end
+    def show
+        student = Student.find(params[:id])
+        render json: student
+    end
 end
